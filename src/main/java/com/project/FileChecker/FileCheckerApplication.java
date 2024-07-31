0aef15e629ef9ff7b904b51607152db37a1d09a7
aditikuls4 <kulshresthaaditi9@gmail.com>
2024-08-01 00:54:43 +0530

    adding security -: now fully resolved

--- src/main/java/com/project/FileChecker/FileCheckerApplication.java
+++ src/main/java/com/project/FileChecker/FileCheckerApplication.java
@@ -1,13 +1,54 @@
 package com.project.FileChecker;
 
+import com.project.FileChecker.entity.Role;
+import com.project.FileChecker.entity.User;
+import com.project.FileChecker.entity.UserRole;
+import com.project.FileChecker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
-public class FileCheckerApplication {
+public class FileCheckerApplication  implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileCheckerApplication.class, args);
 	}
 
+	@Autowired
+	private UserService userService;
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+		BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+
+		User user=new User();
+		user.setFirstName("Admin");
+		user.setLastName("cts");
+		user.setEmail("admin@gmail.com");
+		user.setUsername("admin");
+		user.setPassword(passwordEncoder.encode("admin"));
+		user.setPhoneNo("9123456780");
+		user.setProfile("admin.img");
+
+		Role role1=new Role();
+		role1.setRoleId(44L);
+		role1.setRoleName("ADMIN");
+
+		Set<UserRole> userRoleSet=new HashSet<>();
+		UserRole userRole=new UserRole();
+		userRole.setRole(role1);
+		userRole.setUser(user);
+		userRoleSet.add(userRole);
+
+		User user1=this.userService.createUser(user,userRoleSet);
+		System.out.println(user1.getUsername());
+
+	}
 }
--- src/main/java/com/project/FileChecker/controller/AuthenticateController.java
+++ src/main/java/com/project/FileChecker/controller/AuthenticateController.java
@@ -1,88 +1,75 @@
 package com.project.FileChecker.controller;
 
 
-import com.project.FileChecker.config.javaUtils;
-import com.project.FileChecker.entity.JWTRequest;
-import com.project.FileChecker.entity.JWTResponse;
+
+import java.security.Principal;
+
+import com.project.FileChecker.config.JwtUtil;
+import com.project.FileChecker.entity.JwtRequest;
+import com.project.FileChecker.entity.JwtResponse;
+import com.project.FileChecker.entity.User;
 import com.project.FileChecker.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
-
-    @RestController
-    @CrossOrigin("*")
-    public class AuthenticateController {
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
+@CrossOrigin("*")
+public class AuthenticateController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+    @Autowired
+    private JwtUtil jwtUtil;
 
-        @Autowired
-        private AuthenticationManager authenticationManager;
-
-        @Autowired
-        private UserDetailsServiceImpl userDetailServiceImpl;
-
-        @Autowired
-        private javaUtils jwtutils;
-
-
-        @PostMapping("/generate-token")
-        public ResponseEntity<?> generateToken(@RequestBody JWTRequest jwtRequest) throws Exception {
-
-            try
-            {
-
-                authenticate(jwtRequest.getUsername(),jwtRequest.getPassword());
-
-
-            }
-            catch (UsernameNotFoundException e)
-            {
-                e.printStackTrace();
-                throw new Exception("User Not found");
-            }
-
-            UserDetails userDetails=this.userDetailServiceImpl.loadUserByUsername(jwtRequest.getUsername());
-            System.out.println("user details i.e all the student obj by email"+userDetails);
-            String token=this.jwtutils.generateToken(userDetails);
-
-            return ResponseEntity.ok(new JWTResponse(token));
-
-        }
-
-        private  void  authenticate(String username,String password) throws Exception {
-
-            try {
-                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,password));
-
-
-            }
-            catch (DisabledException e)
-            {
-                throw  new Exception("User disabled");
-            }
-            catch (BadCredentialsException e)
-            {
-                throw  new Exception("Invalid Credentials"+e.getMessage());
-            }
-        }
-
-        @GetMapping("/current-user")
-        public UserDetails getCurrentUser(Principal principal)
+    //generate token
+    @PostMapping("/generate-token")
+    public ResponseEntity<?> generateToken(@RequestBody JwtRequest jwtRequest) throws Exception
+    {
+        try
         {
-            return this.userDetailServiceImpl.loadUserByUsername(principal.getName());
+            authenticate(jwtRequest.getUsername(), jwtRequest.getPassword());
+        }catch (UsernameNotFoundException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            throw new Exception("User not found");
         }
-
+        ///authenticate
+        UserDetails userDetails=this.userDetailsServiceImpl.loadUserByUsername(jwtRequest.getUsername());
+        String token=this.jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
+    private void authenticate(String username,String password) throws Exception
+    {
+        try
+        {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 
+        }catch (DisabledException e) {
+            // TODO: handle exception
+            throw new Exception("USER DISABLED");
+        }catch (BadCredentialsException e) {
+            // TODO: handle exception
+            throw new Exception("Inavlid Credentials "+e.getMessage());
+        }
+    }
+    //return the details of current user
+    @GetMapping("/current-user")
+    public User getCurrentUser(Principal principal)
+    {
+        return ( (User)this.userDetailsServiceImpl.loadUserByUsername(principal.getName()) );
+    }
+}
